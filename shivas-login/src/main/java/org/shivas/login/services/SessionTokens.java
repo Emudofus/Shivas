9573475bdb66fdd341609ddae31c23e34e2ff1c6
Blackrush <blackrushx@gmail.com>
2012-04-07 13:54:36 +0200

    store all attribute's token

---
+++ shivas-login/src/main/java/org/shivas/login/services/SessionTokens.java
@@ -0,0 +1,8 @@
+package org.shivas.login.services;
+
+public class SessionTokens {
+	private SessionTokens() {}
+	
+	public static final String HANDLER = "handler";
+	public static final String TICKET  = "ticket";
+}
