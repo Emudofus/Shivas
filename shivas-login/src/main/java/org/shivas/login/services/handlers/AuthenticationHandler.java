2c652a7daeb43f4b298ecbecffd523be10b75e0e
Blackrush <blackrushx@gmail.com>
2012-04-07 19:32:51 +0200

    improve AuthenticationHandler

--- shivas-login/src/main/java/org/shivas/login/services/handlers/AuthenticationHandler.java
+++ shivas-login/src/main/java/org/shivas/login/services/handlers/AuthenticationHandler.java
@@ -1,23 +1,61 @@
 package org.shivas.login.services.handlers;
 
 import org.apache.mina.core.session.IoSession;
+import org.shivas.common.crypto.CipherException;
 import org.shivas.common.services.IoSessionHandler;
+import org.shivas.login.database.models.Account;
 import org.shivas.login.services.LoginService;
+import org.shivas.login.services.SessionTokens;
+import org.shivas.protocol.client.formatters.LoginMessageFormatter;
 
 public class AuthenticationHandler implements IoSessionHandler<String> {
 	
 	private final IoSession session;
 	private final LoginService service;
+	private final String ticket;
 
 	public AuthenticationHandler(IoSession session, LoginService service) {
 		this.session = session;
 		this.service = service;
+		this.ticket = (String) session.getAttribute(SessionTokens.TICKET);
 	}
 
 	public void init() throws Exception {
 	}
+	
+	private boolean matchedPassword(String input, Account account) throws CipherException {
+		return service.getDecrypter(ticket).cipher(input).equals(account.getPassword());
+	}
 
 	public void handle(String message) throws Exception {
+		if (message.contains("Af")) return; // TODO queue
+		
+		String[] data = message.split("\n");
+		
+		if (data.length != 2) {
+			throw new Exception("bad data received");
+		}
+		
+		String name = data[0], password = data[1].substring(2);
+		
+		Account account = service.getRepositories().getAccounts().findByName(name);
+		
+		if (account == null) {
+			session.write(LoginMessageFormatter.accessDenied());
+		} else if (account.isBanned()) {
+			session.write(LoginMessageFormatter.banned());
+		} else if (!matchedPassword(password, account)) {
+			session.write(LoginMessageFormatter.accessDenied());
+		} else {
+			session.write(LoginMessageFormatter.nicknameInformationMessage(account.getNickname()));
+			// TODO send community
+			// TODO send servers
+			session.write(LoginMessageFormatter.identificationSuccessMessage(account.hasRights()));
+			session.write(LoginMessageFormatter.accountQuestionInformationMessage(account.getSecretQuestion()));
+			
+			// TODO set account's state to connected
+			// TODO set new handler
+		}
 	}
 
 	public void onClosed() {
