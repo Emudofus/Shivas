ed6e7e2d2594b7acd390cac832aba5c3b355db27
Blackrush <blackrushx@gmail.com>
2012-04-10 18:19:12 +0200

    bind RepositoryManager to DefaultRepositoryManager

--- shivas-game/src/main/java/org/shivas/game/ShivasGameModule.java
+++ shivas-game/src/main/java/org/shivas/game/ShivasGameModule.java
@@ -1,5 +1,8 @@
 package org.shivas.game;
 
+import org.shivas.game.database.DefaultRepositoryContainer;
+import org.shivas.game.database.RepositoryContainer;
+
 import com.google.inject.AbstractModule;
 import com.google.inject.persist.jpa.JpaPersistModule;
 
@@ -8,6 +11,8 @@ public class ShivasGameModule extends AbstractModule {
 	@Override
 	protected void configure() {
 		install(new JpaPersistModule(ShivasGameServer.UNIT_PERSIST_NAME));
+		
+		bind(RepositoryContainer.class).to(DefaultRepositoryContainer.class);
 	}
 
 }
