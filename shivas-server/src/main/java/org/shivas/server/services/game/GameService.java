619d39455b4ae77f4b2e7ded694e006c2aa35516
Blackrush <blackrushx@gmail.com>
2012-04-15 15:51:05 +0200

    improve GameService

--- shivas-server/src/main/java/org/shivas/server/services/game/GameService.java
+++ shivas-server/src/main/java/org/shivas/server/services/game/GameService.java
@@ -1,7 +1,10 @@
 package org.shivas.server.services.game;
 
+import org.shivas.protocol.client.types.GameServerType;
 import org.shivas.server.services.Service;
 
 public interface GameService extends Service {
 
+	GameServerType informations();
+	
 }
