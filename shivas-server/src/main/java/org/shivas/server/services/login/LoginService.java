01f02df6eef62c0b5ab4f10b4fc46b921a2747db
Blackrush <blackrushx@gmail.com>
2012-04-15 11:25:31 +0200

    improve LoginService

--- shivas-server/src/main/java/org/shivas/server/services/login/LoginService.java
+++ shivas-server/src/main/java/org/shivas/server/services/login/LoginService.java
@@ -1,7 +1,10 @@
 package org.shivas.server.services.login;
 
+import org.shivas.common.crypto.Cipher;
 import org.shivas.server.services.Service;
 
 public interface LoginService extends Service {
 
+	Cipher makeCipher(String ticket);
+	
 }
