3ca54fa4c5e000af9a567b26d9968aade5464e75
Blackrush <blackrushx@gmail.com>
2012-04-16 12:10:42 +0200

    random pseudo

--- shivas-common/src/main/java/org/shivas/common/StringUtils.java
+++ shivas-common/src/main/java/org/shivas/common/StringUtils.java
@@ -11,13 +11,36 @@ public class StringUtils {
 	public static final String ALPHABET = "abcdefghijklmnopqrstuvwxyz";
 	public static final String EXTENDED_ALPHABET = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_";
 	
+	public static final String VOWELS = "aeiouy";
+	public static final String CONSONANTS = "bcdfghjkmnpqrstvwxz";
+	
 	public static char random() {
 		return ALPHABET.charAt(RAND.get().nextInt(ALPHABET.length()));
+	}
+	
+	public static char randomVowels() {
+		return VOWELS.charAt(RAND.get().nextInt(VOWELS.length()));
+	}
+	
+	public static char randomConsonants() {
+		return CONSONANTS.charAt(RAND.get().nextInt(CONSONANTS.length()));
 	}
 	
 	public static String random(int length) {
 		StringBuilder sb = new StringBuilder(length);
 		for (int i = 0; i < length; ++i) sb.append(random());
+		return sb.toString();
+	}
+	
+	public static String randomPseudo() {
+		boolean vowels = RAND.get().nextBoolean();
+		int length = RAND.get().nextInt(4) + 4;
+		StringBuilder sb = new StringBuilder(length);
+		for (int i = 0; i < length; ++i) {
+			if (vowels) sb.append(randomVowels());
+			else sb.append(randomConsonants());
+			vowels = RAND.get().nextBoolean();
+		}
 		return sb.toString();
 	}
 	
