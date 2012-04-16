8c6323dc9adac26c02f0f45d722caee46068439a
Blackrush <blackrushx@gmail.com>
2012-04-16 20:29:49 +0200

    helper class : handler container

---
+++ shivas-server/src/main/java/org/shivas/server/services/AbstractBaseHandlerContainer.java
@@ -0,0 +1,59 @@
+package org.shivas.server.services;
+
+import java.util.Map;
+
+import org.apache.mina.core.session.IoSession;
+import org.shivas.common.services.IoSessionHandler;
+
+import com.google.common.collect.Maps;
+
+public abstract class AbstractBaseHandlerContainer<C extends Client<?>>
+	extends AbstractBaseHandler<C>
+	implements BaseHandler
+{
+	
+	private Map<Character, BaseHandler> handlers = Maps.newHashMap();
+	private boolean configured;
+
+	public AbstractBaseHandlerContainer(C client, IoSession session) {
+		super(client, session);
+	}
+	
+	protected void add(Character prefix, BaseHandler handler) {
+		handlers.put(prefix, handler);
+	}
+	
+	protected abstract void configure();
+	
+	protected abstract void onReceivedUnknownMessage(String message);
+
+	public IoSessionHandler<String> init() throws Exception {
+		if (!configured) {
+			configure();
+			configured = true;
+		}
+		
+		for (BaseHandler handler : handlers.values()) {
+			handler.init();
+		}
+
+		return this;
+	}
+
+	public void handle(String message) throws Exception {
+		BaseHandler handler = handlers.get(message.charAt(0));
+		
+		if (handler != null) {
+			handler.handle(message);
+		} else {
+			onReceivedUnknownMessage(message);
+		}
+	}
+	
+	public void onClosed() {
+		for (BaseHandler handler : handlers.values()) {
+			handler.onClosed();
+		}
+	}
+
+}
