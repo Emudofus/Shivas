92082581300232e28a261f61942e4c182022f21d
Blackrush <blackrushx@gmail.com>
2012-05-26 12:03:36 +0200

    oubli d'un bind

--- shivas-server/src/main/java/org/shivas/server/database/ShivasDatabaseModule.java
+++ shivas-server/src/main/java/org/shivas/server/database/ShivasDatabaseModule.java
@@ -1,12 +1,8 @@
 package org.shivas.server.database;
 
-import org.atomium.repository.EntityRepository;
-import org.shivas.server.database.models.Account;
-import org.shivas.server.database.models.GameItem;
-import org.shivas.server.database.models.Player;
-import org.shivas.server.database.repositories.AccountRepository;
-import org.shivas.server.database.repositories.GameItemRepository;
-import org.shivas.server.database.repositories.PlayerRepository;
+import org.atomium.repository.*;
+import org.shivas.server.database.models.*;
+import org.shivas.server.database.repositories.*;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.TypeLiteral;
@@ -18,6 +14,7 @@ public class ShivasDatabaseModule extends AbstractModule {
 		bind(new TypeLiteral<EntityRepository<Integer, Account>>(){}).to(AccountRepository.class);
 		bind(new TypeLiteral<EntityRepository<Integer, Player>>(){}).to(PlayerRepository.class);
 		bind(new TypeLiteral<EntityRepository<Long, GameItem>>(){}).to(GameItemRepository.class);
+		bind(new TypeLiteral<PersistableEntityRepository<Long, GameItem>>(){}).to(GameItemRepository.class);
 	}
 
 }
