be933a5998e3096b04436ce36f8e29cec78c7aee
Blackrush <blackrushx@gmail.com>
2012-07-31 10:38:49 +0200

    GiveKamasAction : nouvelle action d'objet utilisable

---
+++ shivas-server/src/main/java/org/shivas/server/core/items/actions/GiveKamasAction.java
@@ -0,0 +1,46 @@
+package org.shivas.server.core.items.actions;
+
+import java.util.Map;
+
+import org.shivas.data.entity.ItemAction;
+import org.shivas.server.database.models.Player;
+import org.shivas.server.services.game.GameClient;
+
+public class GiveKamasAction implements ItemAction {
+
+	public static final int TYPE = 2;
+	
+	public static GiveKamasAction make(Map<String, String> parameters) {
+		String raw = parameters.get("kamas");
+		long kamas = Long.parseLong(raw);
+		
+		return new GiveKamasAction(kamas);
+	}
+	
+	private final long kamas;
+
+	public GiveKamasAction(long kamas) {
+		this.kamas = kamas;
+	}
+
+	@Override
+	public int getType() {
+		return TYPE;
+	}
+
+	@Override
+	public boolean able(Object target) {
+		return target instanceof Player;
+	}
+
+	@Override
+	public void apply(Object arg0) {
+		Player target = (Player) arg0;
+		GameClient client = target.getClient();
+		
+		target.getBag().plusKamas(kamas);
+		
+		client.write(target.getStats().packet());
+	}
+
+}
