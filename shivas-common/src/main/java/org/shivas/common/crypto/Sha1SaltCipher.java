c0980f710298f187e0660a0f355d089af0d227c2
Blackrush <blackrushx@gmail.com>
2012-08-25 16:32:36 +0200

    ajout d'un nouveau crypteur

---
+++ shivas-common/src/main/java/org/shivas/common/crypto/Sha1SaltCipher.java
@@ -0,0 +1,47 @@
+package org.shivas.common.crypto;
+
+import java.security.NoSuchAlgorithmException;
+
+public class Sha1SaltCipher extends Sha1Cipher {
+	
+	private static byte[] concat(byte[] first, byte[] second) {
+		int i = 0, nb = first.length + second.length;
+		byte[] result = new byte[nb];
+		for (; i < first.length; ++i) {
+			result[i] = first[i];
+		}
+		for (; i < nb; ++i) {
+			result[i] = second[i - first.length];
+		}
+		return result;
+	}
+	
+	private String salt;
+	private byte[] bytes;
+
+	public Sha1SaltCipher(String salt) throws NoSuchAlgorithmException {
+		super();
+		
+		this.salt = salt;
+		this.bytes = salt.getBytes();
+	}
+
+	@Override
+	public byte[] cipher(byte[] bytes) throws CipherException {
+		byte[] first = super.cipherString(bytes).getBytes();
+		return super.cipher(concat(first, this.bytes));
+	}
+
+	@Override
+	public String cipher(String string) throws CipherException {
+		String first = super.cipher(string);
+		return super.cipher(first + salt);
+	}
+
+	@Override
+	public String cipherString(byte[] bytes) throws CipherException {
+		byte[] first = super.cipherString(bytes).getBytes();
+		return super.cipherString(concat(first, this.bytes));
+	}
+
+}
