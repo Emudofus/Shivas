3484c933052bd7409551b936372a05478e0517c3
Blackrush <blackrushx@gmail.com>
2012-09-05 11:57:13 +0200

    sûrement des erreurs, c'est juste pour se faire une idée

--- plugins/pub_manager.groovy
+++ plugins/pub_manager.groovy
@@ -1,28 +1,27 @@
 import org.shivas.server.core.plugins.Plugin
 import org.shivas.server.core.plugins.Startable
+import org.shivas.server.core.commands.Command
+import org.shivas.common.params.Types
 import com.google.common.collect.Lists
 import javax.swing.Timer
 
 class PubManagerPlugin extends Plugin {
-	String getAuthor() {
-		return "Blackrush"
-	}
-
-	String getVersion() {
-		return "alpha1"
-	}
-
-	String getHelp() {
-		return "Sends pub message in global channel"
-	}
+	final String author = "Blackrush"
+	final String version = "alpha1"
+	final String help = "Sends pub message in global channel"
 
 	Collection<Startable> getServices() {
-		return Lists.newArrayList(new PubManagerService())
+		return Lists.newArrayList(PubManagerService.INSTANCE)
+	}
+
+	Collection<Command> getCommands() {
+		return Lists.newArrayList(new PubManagerCommand())
 	}
 }
 
 class PubManagerService implements Startable {
 	static final int RATE = 2 * 60
+	static final PubManagerService INSTANCE = new PubManagerService()
 
 	private Timer timer
 
@@ -45,6 +44,35 @@ class PubManagerService implements Startable {
 
 	void stop() {
 		timer.stop()
+	}
+}
+
+class PubManagerCommand implements Command {
+	String name() {
+		return "pub"
+	}
+
+	Conditions conditions() {
+		Conditions conds = new Conditions()
+		conds.add("msg", Types.STRING, "The message that PubManagerService will sent")
+
+		return conds
+	}
+
+	String help() {
+		return "Set the pub message"
+	}
+	
+	boolean canUse(GameClient client) {
+		return client.account().hasRights()
+	}
+
+	void use(GameClient client, DofusLogger log, Parameters params) {
+		String msg = params.get("msg", String)
+
+		PubManagerService.INSTANCE.pubMessage = msg
+
+		log.info("the message has been setted")
 	}
 }
 
