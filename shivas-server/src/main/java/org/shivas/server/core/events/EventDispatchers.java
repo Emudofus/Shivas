53faee675ba9a88cb01f27b09ee34dd5c4ae3957
Blackrush <blackrushx@gmail.com>
2012-09-12 18:50:46 +0200

    jolie ConcurrentModificationException

--- shivas-server/src/main/java/org/shivas/server/core/events/EventDispatchers.java
+++ shivas-server/src/main/java/org/shivas/server/core/events/EventDispatchers.java
@@ -1,14 +1,9 @@
 package org.shivas.server.core.events;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
 public final class EventDispatchers {
 	private EventDispatchers(){}
 	
-	private static final ExecutorService executor = Executors.newSingleThreadExecutor();
-	
 	public static EventDispatcher create(){
-		return new ThreadedEventDispatcher(executor);
+		return new ThreadedEventDispatcher();
 	}
 }
--- shivas-server/src/main/java/org/shivas/server/core/events/ThreadedEventDispatcher.java
+++ shivas-server/src/main/java/org/shivas/server/core/events/ThreadedEventDispatcher.java
@@ -1,23 +1,51 @@
 package org.shivas.server.core.events;
 
+import java.util.List;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
-public class ThreadedEventDispatcher extends AbstractEventDispatcher {
+import com.google.common.collect.Lists;
+
+public class ThreadedEventDispatcher implements EventDispatcher {
 	
-	private ExecutorService worker;
+	private ExecutorService worker = Executors.newSingleThreadExecutor();
+	
+	private final List<EventListener> listeners = Lists.newArrayList();
 
-	public ThreadedEventDispatcher(ExecutorService worker) {
-		this.worker = worker;
-	}
-
+	@Override
 	public void publish(final Event event) {
 		worker.execute(new Runnable() {
 			public void run() {
-				lock.lock();
 				for (EventListener listener : listeners) {
 					listener.listen(event);
 				}
-				lock.unlock();
+			}
+		});
+	}
+
+	@Override
+	public void subscribe(final EventListener listener) {
+		worker.execute(new Runnable() {
+			public void run() {
+				listeners.add(listener);
+			}
+		});
+	}
+
+	@Override
+	public void unsubscribe(final EventListener listener) {
+		worker.execute(new Runnable() {
+			public void run() {
+				listeners.remove(listener);
+			}
+		});
+	}
+
+	@Override
+	public void clear() {
+		worker.execute(new Runnable() {
+			public void run() {
+				listeners.clear();
 			}
 		});
 	}
