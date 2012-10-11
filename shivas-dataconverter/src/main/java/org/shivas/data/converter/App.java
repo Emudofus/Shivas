7208452cfab849cc0439d594eb768ea047f4fb94
Blackrush <blackrushx@gmail.com>
2012-10-11 09:02:28 +0200

    quelques fonctions bien utiles

--- shivas-dataconverter/src/main/java/org/shivas/data/converter/App.java
+++ shivas-dataconverter/src/main/java/org/shivas/data/converter/App.java
@@ -1,7 +1,33 @@
 package org.shivas.data.converter;
 
+import java.util.Scanner;
+
 public class App {
-	
+
+    private static Scanner scan = new Scanner(System.in);
+    private static String CONFIRMATION_STR = "oui";
+
+    public static void out(String format, Object... args) {
+        System.out.format(format, args);
+    }
+
+    public static void outln(String format, Object... args) {
+        System.out.format(format + "\n", args);
+    }
+
+    public static String readln() {
+        return scan.nextLine();
+    }
+
+    public static String prompt(String format, Object... args) {
+        out(format, args);
+        return readln();
+    }
+
+    public static boolean confirmation(String format, Object... args) {
+        return prompt(format, args).equalsIgnoreCase(CONFIRMATION_STR);
+    }
+
     public static void main(String[] args) {
     }
 }
