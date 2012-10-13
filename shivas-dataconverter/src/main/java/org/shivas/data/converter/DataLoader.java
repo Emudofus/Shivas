f868b7e809704ea24b6836cd24e5610891f0d019
Blackrush <blackrushx@gmail.com>
2012-10-13 14:13:43 +0200

    création des dataloaders

---
+++ shivas-dataconverter/src/main/java/org/shivas/data/converter/DataLoader.java
@@ -0,0 +1,24 @@
+package org.shivas.data.converter;
+
+import org.shivas.data.entity.*;
+
+import java.util.Collection;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Blackrush
+ * Date: 13/10/12
+ * Time: 13:59
+ */
+public interface DataLoader {
+
+    Collection<Breed> loadBreeds() throws Exception;
+    Collection<Experience> loadExperiences() throws Exception;
+    Collection<MapData> loadMaps() throws Exception;
+    Collection<ItemSet> loadItemSets() throws Exception;
+    Collection<ItemTemplate> loadItems() throws Exception;
+    Collection<SpellTemplate> loadSpells() throws Exception;
+    Collection<Waypoint> loadWaypoints() throws Exception;
+    Collection<NpcTemplate> loadNpcTemplates() throws Exception;
+
+}
